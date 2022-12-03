d0ae0dddc9678afb4b2726d61ca231829c5e621d
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-03 15:05:03 +0800

    2

--- sum.js
+++ sum.js
@@ -1,13 +1,16 @@
 
 
 function sum(num) {
-    let n = num;
     let sum = 0;
-    for (let i = 0; i <= n; i++) {
-        sum+=i;
-        
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+
     }
     console.log(sum)
 }
-sum(0)
+
+
+sum(-1)
 sum(10)
+
+
