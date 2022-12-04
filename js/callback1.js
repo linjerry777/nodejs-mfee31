5c95cb7aad13520e5366d8eb7a8c8f8f448db2a3
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-04 16:57:56 +0800

    ==

--- js/callback1.js
+++ js/callback1.js
@@ -1,27 +1,31 @@
 let doWork = function (job, timer, cb) {
-    let new2timer = 0
-
-    for (let i = 0; i < job.length; i++) {
-
-        let newjob = job[i];
-        let newtimer = timer[i];
-        new2timer += newtimer;
-
-
-
-        setTimeout(() => {
-            let now = new Date();
-            cb(null, `完成工作 ${newjob} at ${now.toISOString()}`);
-        }, new2timer);
+    setTimeout(() => {
+      let now = new Date();
+      cb(null, `完成工作 ${job} at ${now.toISOString()}`);
+    }, timer);
+  };
+  
+  let now = new Date();
+  console.log(`工作開始 at ${now.toISOString()}`);
+  // 刷牙 3 秒鐘 -> 吃早餐 5 秒鐘 -> 寫功課 3 秒鐘
+  doWork('刷牙', 3000, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(data);
+      doWork('吃早餐', 5000, (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(data);
+          doWork('寫功課', 3000, (err, data) => {
+            if (err) {
+              console.error(err);
+            } else {
+              console.log(data);
+            }
+          });
+        }
+      });
     }
-
-
-};
-
-let now = new Date();
-console.log(`工作開始 at ${now.toISOString()}`);
-// 刷牙 3 秒鐘 -> 吃早餐 5 秒鐘 -> 寫功課 3 秒鐘
-doWork(['刷牙', '早餐', '上班'], [3000, 5000, 3000], (err, data) => {
-    console.log(data);
-
-});
+  });
