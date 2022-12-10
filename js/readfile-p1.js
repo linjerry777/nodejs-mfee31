11996ff492cdbce4534f9552e96fb15971c1912c
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-10 15:04:39 +0800

    await寫法

---
+++ js/readfile-p1.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+// error-first callback
+/* fs.readFile("test.txt", "utf-8", (err, data) => {
+  if (err) {
+    // 如果 err 有值，表示有錯誤發生
+    // 這裡應該要處理錯誤
+    console.error("發生錯誤了", err);
+  } else {
+    // 進來這裡，表示 err 是空的 (可能是 null)
+    console.log("成功讀到資料:", data);
+  }
+}); */
+
+const p = function (fileName) {
+  //宣告一個function方便調用(fileName用來接收要開的檔案)
+  let doreadFile = new Promise((resolve, reject) => {
+    //let xx = new Promise((resolve, reject) =>{}) Promise語法糖
+
+    fs.readFile(fileName, "utf-8", (err, data) => {
+      if (err) {
+        // 如果 err 有值，表示有錯誤發生
+        // 這裡應該要處理錯誤
+        // console.error();
+        reject("發生錯誤了", err);
+      } else {
+        resolve(`成功讀到資料: ${data}`);
+        // 進來這裡，表示 err 是空的 (可能是 null)
+        // console.log("成功讀到資料:", data);
+      }
+
+      //條件判斷 resolve or reject
+    });
+  });
+  //返回promise物件
+  return doreadFile;
+};
+
+
+async function read() {
+  try {
+    console.log(await p("test.txt"));
+    console.log(await p("test1.txt"));
+    console.log(await p("test2.txt"));
+    console.log(await p("test3.txt"));
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+read();
