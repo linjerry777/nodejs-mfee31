5c0e0cfdf098142a7c262b27a4546aadeb201d4c
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-10 12:24:25 +0800

    抓多個txt

--- js/readfile-p.js
+++ js/readfile-p.js
@@ -12,9 +12,9 @@ const fs = require("fs");
   }
 }); */
 
-const p = function () {
+const p = function (fileName) {
   let doreadFile = new Promise((resolve, reject) => {
-    fs.readFile("test.txt", "utf-8", (err, data) => {
+    fs.readFile(fileName, "utf-8", (err, data) => {
       if (err) {
         // 如果 err 有值，表示有錯誤發生
         // 這裡應該要處理錯誤
@@ -29,10 +29,20 @@ const p = function () {
   });
   return doreadFile;
 };
-p()
+
+p('test.txt')
   .then((data) => {
     console.log("收到", data);
+   return p('test1.txt')
+  })
+  .then((data) => {
+    console.log("收到", data);
+    return p('test2.txt')
+  })
+  .then((data) => {
+    console.log("收到", data)
   })
   .catch((err) => {
     console.log("沒收到", err);
-  });
+  })
+  
