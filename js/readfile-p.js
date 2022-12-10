cd42d3dc0f2ce4f68f8aa110bb72f4774061df5a
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-10 13:00:06 +0800

    抓å修改註釋

--- js/readfile-p.js
+++ js/readfile-p.js
@@ -13,7 +13,10 @@ const fs = require("fs");
 }); */
 
 const p = function (fileName) {
+  //宣告一個function方便調用(fileName用來接收要開的檔案)
   let doreadFile = new Promise((resolve, reject) => {
+    //let xx = new Promise((resolve, reject) =>{}) Promise語法糖
+
     fs.readFile(fileName, "utf-8", (err, data) => {
       if (err) {
         // 如果 err 有值，表示有錯誤發生
@@ -25,14 +28,19 @@ const p = function (fileName) {
         // 進來這裡，表示 err 是空的 (可能是 null)
         // console.log("成功讀到資料:", data);
       }
+
+      //條件判斷 resolve or reject
     });
   });
+  //返回promise物件
   return doreadFile;
 };
 
+//調用function('filename') data 為resolve裡的值
 p('test.txt')
   .then((data) => {
     console.log("收到", data);
+    //callback再次調用p function(filename)
    return p('test1.txt')
   })
   .then((data) => {
