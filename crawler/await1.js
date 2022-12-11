f4a691f0fcafc024b84feffc2bef159a9e9d13e7
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-11 16:02:30 +0800

    可寫入不同txt

--- crawler/await1.js
+++ crawler/await1.js
@@ -5,8 +5,10 @@ const axios = require('axios');
 // fs.readFile('test.txt', 'utf-8', (err, data) => {});
 // new Promise(...)
 
-let readFilePromise = util.promisify(fs.readFile);
-readFilePromise('stock.txt', 'utf-8')
+const getStock = function(fileName){
+    let readFilePromise = util.promisify(fs.readFile);
+
+readFilePromise(fileName, 'utf-8')
   .then((data) => {
     console.log(data);
     (async () => {
@@ -28,6 +30,35 @@ readFilePromise('stock.txt', 'utf-8')
 
   })
   .catch();
+}
+
+getStock('stock.txt')
+/* let readFilePromise = util.promisify(fs.readFile);
+
+readFilePromise('stock.txt', 'utf-8')
+  .then((data) => {
+    console.log(data);
+    (async () => {
+        try {
+          let stockNo = data;
+          let date = '20221111';
+          let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
+            params: {
+              stockNo,
+              date,
+            },
+          });
+      
+          console.log('await', response.data);
+        } catch (e) {
+          console.error(e);
+        }
+      })();
+
+  })
+  .catch(); */
+
+
 
   
   
