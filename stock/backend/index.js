1b8742b4aa4a7a068645bfae316b672e656b63c3
linjerry777 <iii20220906linjerry@gmail.com>
2022-12-17 15:10:25 +0800

    express and nodemon

--- stock/backend/index.js
+++ stock/backend/index.js
@@ -1,1 +1,10 @@
 import express from "express";
+
+const app = express();
+app.listen(8800,(req,res)=>{
+    console.log("連到囉~")
+})
+app.get("/", (req, res) => {
+  res.json("hello this is backend");
+
+});
